d9c7cf3130603ab1879f7513df21de8a77ce0704
Ryan Spletzer <ryan.spletzer@autodesk.com>
2023-05-22 22:09:32 -0400

    remove ISE profiles

--- Documents/WindowsPowerShell/Microsoft.PowerShellISE_profile.ps1
+++
@@ -1,6 +0,0 @@
-$originalProfilePath = (
-    Resolve-Path -Path "$env:USERPROFILE/Documents/PowerShell/Microsoft.PowerShell_profile.ps1"
-).Path
-if (-not ($PSScriptRoot -eq $originalProfilePath)) {
-    . $originalProfilePath
-}
